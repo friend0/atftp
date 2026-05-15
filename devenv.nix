b83844f266bc6132bc25444513b6381a94500555
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2026-05-15 01:26:52 -0700

    project bootstrapping

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+      "rust-src"
+    ];
+  };
+
+  enterShell = ''
+    rustc --version
+    cargo --version
+  '';
+}
